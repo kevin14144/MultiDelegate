9b100890e57ee341a906327ef84fdacf45ba8cee
asus-PC\asus <kevin14144@yahoo.com.tw>
2017-05-23 23:32:23 +0800

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,15 +15,19 @@ MainWindow::~MainWindow()
 
 int (*pf)(int);
 typedef int (*MathFunc)(float, int);
-int call_a_func(MathFunc call_this) {
+
+int call_a_func(MathFunc call_this)
+{
     int output = call_this(5.5, 7);
     return output;
 }
 
-int call_a_funcs(int (*call_this)(float, int)) {
+int call_a_funcs(int (*call_this)(float, int))
+{
     int output = call_this(5.5, 7);
     return output;
 }
+
 int do_math(float arg1, int arg2)
 {
     return arg2;
